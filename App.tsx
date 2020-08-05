482991515aaf059d91108584a224e4849ef59525
Emilio Franco <ejfranco06@gmail.com>
2020-08-05 09:42:37 -0400

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>Buy me</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
